6f70ae4e0458a13511d0cc08132775480702057b
singhvishal14 <kumarvishalsingh767@gmail.com>
2023-07-11 11:47:01 +0530

    third commit

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = 5;//Value Type
+            Console.WriteLine(num);
+            object obj; //Reference Type
+            obj = num;//Boxing 
+            Console.WriteLine(obj);
+
+            int num2;
+            num2 = (int)obj; //UnBoxing
+            Console.WriteLine(num2);
+            Console.ReadKey();
+
+
+        }
+    }
+}
